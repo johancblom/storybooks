b4e7acddd060ad1807c0d96b5feab069fc98a300
Johan Blom <Jblom@london.flextrade.com>
2018-04-18 21:12:13 +0100

    fixed a few issues with twitter

--- config/passport.js
+++ config/passport.js
@@ -5,54 +5,88 @@ const keys = require('./keys');
 // Load user model
 const User = mongoose.model('users');
 
-module.exports = function(passport){
+module.exports = function(passport) {
   passport.use(
-    new GoogleStrategy({
-      clientID: keys.googleClientID,
-      clientSecret:keys.googleClientSecret,
-      callbackURL:'/auth/google/callback',
-      proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-      // console.log(accessToken);
-      // console.log(profile);
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
+        proxy: true
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // console.log(accessToken);
+        // console.log(profile);
 
-      const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
-      
-      const newUser = {
-        googleID: profile.id,
-        firstName: profile.name.givenName,
-        lastName: profile.name.familyName,
-        email: profile.emails[0].value,
-        image: image
+        const image = profile.photos[0].value.substring(
+          0,
+          profile.photos[0].value.indexOf('?')
+        );
+
+        const newUser = {
+          googleID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          image: image
+        };
+
+        // Check for existing user
+        User.findOne({
+          googleID: profile.id
+        }).then(user => {
+          if (user) {
+            // Return user
+            done(null, user);
+          } else {
+            // Create user
+            new User(newUser).save().then(user => done(null, user));
+          }
+        });
       }
-
-      // Check for existing user
-      User.findOne({
-        googleID: profile.id
-      }).then(user => {
-        if(user){
-          // Return user
-          done(null, user);
-        } else {
-          // Create user
-          new User(newUser)
-            .save()
-            .then(user => done(null, user));
-        }
-      })
-    })
+    )
   );
 
-  passport.use(new TwitterStrategy({
-    consumerKey: keys.twitterClientID,
-    consumerSecret: keys.twitterClientSecret,
-    callbackURL: '/auth/twitter/callback' //this will need to be dealt with
-    }, function(token, tokenSecret, profile, done) {
-      console.log(profile);
-      process.nextTick(function () {
-        return done(null, profile);
-      });
-    }));
+  passport.use(
+    new TwitterStrategy(
+      {
+        consumerKey: keys.twitterClientID,
+        consumerSecret: keys.twitterClientSecret,
+        callbackURL: '/auth/twitter/callback' //this will need to be dealt with
+      },
+      function(token, tokenSecret, profile, done) {
+        console.log(profile);
+        const image = profile.photos[0].value;
+
+        console.log(image);
+
+        const newUser = {
+          googleID: profile.id,
+          firstName: profile.displayName.substring(
+            0,
+            profile.displayName.indexOf(' ')
+          ),
+          lastName: profile.displayName.substring(
+            profile.displayName.indexOf(' ')
+          ),
+          email: 'a@b.com',
+          image: image
+        };
+
+        User.findOne({
+          googleID: profile.id
+        }).then(user => {
+          if (user) {
+            // Return user
+            done(null, user);
+          } else {
+            // Create user
+            new User(newUser).save().then(user => done(null, user));
+          }
+        });
+      }
+    )
+  );
 
   passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -61,4 +95,4 @@ module.exports = function(passport){
   passport.deserializeUser((id, done) => {
     User.findById(id).then(user => done(null, user));
   });
-}
+};
